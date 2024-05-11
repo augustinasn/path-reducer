556cf4a1f05a9b466fa8e0c340dd10e851b30e95
Augustinas Naina <augustinas@naina.lt>
2024-05-11 17:01:07 +0300

    cosmetic changes for publishing to pypi

--- test.py
+++ test.py
@@ -1,24 +1,7 @@
 import unittest
-from path_reducer_an.path_reducer import PathReducer
-
-
-class TestPathReducer(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.path_reducer = PathReducer()
-
-    def test_case_1(self):
-        self.assertEqual(self.path_reducer.reduce_path(["NORTH","SOUTH","SOUTH","EAST","WEST","NORTH","WEST"]), ["WEST"])
-
-    def test_case_2(self):
-        self.assertEqual(self.path_reducer.reduce_path(["NORTH","SOUTH","SOUTH","EAST","WEST","NORTH"]), [])
-
-    def test_case_3(self):
-        self.assertEqual(self.path_reducer.reduce_path(["NORTH","WEST","SOUTH","EAST"]), ["NORTH","WEST","SOUTH","EAST"])
-
-    def test_case_4(self):
-        self.assertEqual(self.path_reducer.reduce_path(["NORTH","SOUTH","EAST","WEST"]), [])
+from path_reducer.path_reducer import PathReducer
+from tests.test_path_reducer import TestPathReducer
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
